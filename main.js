b487be0fc6c77d5caa21fad61670ebce690f0312
Iwasson <Iwasson@users.noreply.github.com>
2021-02-11 23:54:27 -0800

    Starting some functions
    Got first two functions working

--- main.js
+++ main.js
@@ -1,48 +1,216 @@
-const tmi = require('tmi.js');
 const auth = require('./auth.json');
+const axios = require('axios');
+const fs = require('fs');
+const temList = require('./temtem.json');
+const url = 'https://temtem-api.mael.tech/api/';
 
-// Define configuration options
-const opts = {
-  identity: {
-    username: auth.username,
-    password: auth.accessToken
-  },
-  channels: [
-    auth.channels
-  ]
-};
-// Create a client with our options
-const client = new tmi.client(opts);
 
-// Register our event handlers (defined below)
-client.on('message', onMessageHandler);
-client.on('connected', onConnectedHandler);
 
-// Connect to Twitch:
-client.connect();
-
-// Called every time a message comes in
-function onMessageHandler (target, context, msg, self) {
-  if (self) { return; } // Ignore messages from the bot
-
-  // Remove whitespace from chat message
-  const commandName = msg.trim();
-
-  // If the command is known, let's execute it
-  if (commandName === '!dice') {
-    const num = rollDice();
-    client.say(target, `You rolled a ${num}`);
-    console.log(`* Executed ${commandName} command`);
-  } else {
-    console.log(`* Unknown command ${commandName}`);
+function menu(input) {
+  let command = input.split(" ");
+  console.log('command: ' + command);
+  
+  switch (command[0]) {
+    /* with over 130 tems, will bomb stream chat */
+    case "getTems":
+      getAllTem();
+      break;
+    case "getTem":
+      getTemInfo(command[1]);
+      break;
+    case "freeTem":
+      freeTem(command[1], command[2]);
+      break;
+    case "temRewards":
+      temRewards();
+      break;
+    case "types":
+      types();
+      break;
+    case "conditions":
+      conditions();
+      break;
+    case "techniques":
+      techniques();
+      break;
+    case "courses":
+      courses();
+      break;
+    case "traits":
+      traits();
+      break;
+    case "items":
+      items();
+      break;
+    case "gear":
+      gear();
+      break;
+    case "quests":
+      quests();
+      break;
+    case "dojos":
+      dojos();
+      break;
+    case "characters":
+      characters();
+      break;
+    case "saipark":
+      saipark();
+      break;
+    case "locations":
+      locations();
+      break;
+    case "cosmetics":
+      cosmetics();
+      break;
+    case "dyes":
+      dyes();
+      break;
+    case "patches":
+      patches();
+      break;
+    case "weaknesses":
+      weaknesses();
+      break;
+    case "weakCalc":
+      weakCalc();
+      break;
+    default:
+      console.log("Please provide correct input");
+      break;
   }
 }
-// Function called when the "dice" command is issued
-function rollDice () {
-  const sides = 6;
-  return Math.floor(Math.random() * sides) + 1;
+
+// '/api/temtems'
+//Admin command that updates a list of all tems.
+async function getAllTem() {
+  axios.get(url + 'temtems')
+    .then(function (response) {
+      let names = "{";
+      response.data.forEach(tem => {
+        names += "\"" + tem.name.toLowerCase() + "\"" + ":" + "\"" + tem.number + "\",";
+      });
+      names = names.substring(0, names.length-1);
+      names += "}";
+      fs.writeFile("temtem.json", names, function (err){
+        if(err) { console.log(err) }
+      })
+      console.log("List saved!");
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+    return;
 }
-// Called every time the bot connects to Twitch chat
-function onConnectedHandler (addr, port) {
-  console.log(`* Connected to ${addr}:${port}`);
+
+// '/api/temtems/[number]'
+function getTemInfo(temName) {
+  let temNum = temList[temName.toLowerCase()];
+  axios.get(url + 'temtems/' + temNum)
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.log("could not find tem!");
+    })
+    return;
 }
+
+// '/api/freetem/[temtem]/[level]'
+async function freeTem(temName, level) {
+
+}
+
+// '/api/freetem/rewards'
+async function temRewards() {
+
+}
+
+// '/api/types'
+async function types() {
+
+}
+
+// '/api/conditions'
+async function conditions() {
+
+}
+
+// '/api/techniques'
+async function techniques() {
+
+}
+
+// '/api/training-courses'
+async function courses() {
+
+}
+
+// '/api/traits'
+async function traits() {
+
+}
+
+// '/api/items'
+async function items() {
+
+}
+
+// '/api/gear'
+async function gear() {
+
+}
+
+// '/api/quests'
+async function quests() {
+
+}
+
+// '/api/dojos'
+async function dojos() {
+  
+}
+
+// '/api/characters'
+async function characters() {
+
+}
+
+// '/api/saipark'
+async function saipark() {
+
+}
+
+// '/api/locations'
+async function locations() {
+
+}
+
+// '/api/cosmetics'
+async function cosmetics() {
+
+}
+
+// '/api/dyes'
+async function dyes() {
+
+}
+
+// '/api/patches'
+async function patches() {
+
+}
+
+// '/api/weaknesses'
+async function weaknesses() {
+
+}
+
+// '/api/weaknesses/calculate'
+async function weakCalc() {
+
+}
+
+//menu("getTems");
+menu('getTem Oree');
+//console.log(temList.oree);
